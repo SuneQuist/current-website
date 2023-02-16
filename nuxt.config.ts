c479c90933fdbba89f5902a1ebb3c1e23874c006
SuneQuist <sunevinterhoej@gmail.com>
2023-02-16 01:34:06 +0100

    favicon and manifest added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,13 +13,49 @@ export default defineNuxtConfig({
     },
     app: {
         head: {
+            title: 'sune',
+            meta: [
+              { charset: 'utf-8' },
+              { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+              {
+                hid: 'description',
+                name: 'description',
+                content: 'my website description'
+              }
+            ],
             link: [
+                { 
+                    rel: 'icon', 
+                    type: 'image/x-icon', 
+                    href: '/favicon.ico' 
+                },
+                { 
+                    rel: 'icon', 
+                    type: 'image/png',
+                    sizes: "16x16",
+                    href: '/favicon-16x16.png' 
+                },
+                { 
+                    rel: 'icon', 
+                    type: 'image/png',
+                    sizes: "32x32",
+                    href: '/favicon-32x32.png' 
+                },
+                { 
+                    rel: 'apple-touch-icon', 
+                    sizes: "180x180",
+                    href: '/apple-touch-icon.png' 
+                },
+                { 
+                    rel: 'manifest', 
+                    href: '/site.webmanifest' 
+                },
                 {
                     rel: "stylesheet",
                     href: "//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/styles/default.min.css"
                 }
             ]
-        }
+          }
     },
     components: [
         {
