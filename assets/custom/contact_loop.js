8e19eeac16e19fcdf4fed147a58c352486aa7c3a
SuneQuist <sunevinterhoej@gmail.com>
2023-02-19 03:38:21 +0100

    hope?

--- assets/custom/contact_loop.js
+++ assets/custom/contact_loop.js
@@ -1,21 +1,11 @@
-let images = [];
-
-(function() {
-    onNuxtReady(() => {
-        for (let i = 0; i < 26; i++ ) {
-            const img = new Image();
-            img.src = `/images/contact/Artboard${i}.png`;
-            img.onload = () => { images.push(img) };
-        }
-    })
-}())
-
 let index = 0;
 let active = true;
 let timeout;
 let frames = 8;
 export function runAnimationLoop(el) {
-    onNuxtReady(() => {        
+    onNuxtReady(() => {
+        clearInterval(timeout);
+        
         if (active) {
             timeout = setInterval(() => { 
                 if (index >= 26) { 
@@ -23,13 +13,12 @@ export function runAnimationLoop(el) {
                         index = 0;
                     }, 1000);
                 } else {
-                    el.target.src = images[index].src; 
+                    el.target.src = `/images/contact/Artboard${index}.png`; 
                     index++; 
                 }
             }, 1000/frames);
         }
     });
 
-    clearInterval(timeout);
     active = !active;
 }
